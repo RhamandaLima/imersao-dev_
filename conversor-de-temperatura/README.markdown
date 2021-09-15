a463220a8546728d03142ffefe58f7e66a7e81a8
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-09-15 19:13:23 -0300

    Add vers]ao 2.0

--- conversor-de-temperatura/README.markdown
+++ conversor-de-temperatura/README.markdown
@@ -2,4 +2,4 @@
 
 A Pen created on CodePen.io. Original URL: [https://codepen.io/rhamandalima/pen/PojJmbo](https://codepen.io/rhamandalima/pen/PojJmbo).
 
-
+Projeto elaborado a partir do desafio da aula 01 da Imersão Dev_ da Alura.
--- conversor-de-temperatura/src/script.js
+++ conversor-de-temperatura/src/script.js
@@ -1,10 +1,18 @@
-var tempEmC= document.querySelector('.valorC'); 
-var tempEmF= document.querySelector('.valorF');
+var tempEmC=document.querySelector('.valorC'); 
+var tempEmF=document.querySelector('.valorF');
+var tempEmK=document.querySelector('.valorK');
 
 tempEmC.addEventListener('input', function(event){
-  tempEmF.value = (tempEmC.value * 1.8) + 32 + " ºF";
+  tempEmF.value = ((tempEmC.value * 1.8) + 32).toFixed(2);
+  tempEmK.value = (parseFloat(tempEmC.value) + 273.15).toFixed(2);
 }, false);
 
 tempEmF.addEventListener('input', function(event){
-   tempEmC.value  = (tempEmF.value -32) / 1.8 + " ºC";
+   tempEmC.value  =  ((tempEmF.value - 32) / 1.8).toFixed(2);
+   tempEmK.value =  ((tempEmF.value - 32) * 5/9 + 273.15).toFixed(2);
+}, false);
+
+tempEmK.addEventListener('input', function(event){
+  tempEmF.value =  ((tempEmK.value - 273.15) * 1.8 + 32).toFixed(2);
+  tempEmC.value = (tempEmK.value - 273.15).toFixed(2);
 }, false);
