901a74c0e113e8aebf7992e8f4b91f43b043d565
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-09-18 20:23:17 -0300

    Add Conversor de Moedas versão 2.0

--- conversor-de-moedas-id3/README.markdown
+++ conversor-de-moedas-id3/README.markdown
@@ -1,5 +1,6 @@
-# Conversor de moedas - ID3
+# Conversor de moedas - Versão 2.0
 
-A Pen created on CodePen.io. Original URL: [https://codepen.io/rhamandalima/pen/zYzEJpo](https://codepen.io/rhamandalima/pen/zYzEJpo).
+A Pen created on CodePen.io. Original URL: [https://codepen.io/rhamandalima/pen/QWgQJJv](https://codepen.io/rhamandalima/pen/QWgQJJv).
 
-Projeto elaborado a partir do desafio da aula 02 da Imersão Dev_ da Alura.
+Projeto elaborado a partir do desafio da aula 05 da Imersão Dev_ da Alura.
+Desafio: "Conversor de moedas: criar funções para cada tipo de conversão e chamá-las dependendo do que você quiser fazer, podendo colocar inputs ou botões e quando clicar chamar cada função converteDolar(), converteReal(), converteEuro() e converteBitcoin(), por exemplo."
--- conversor-de-moedas-id3/dist/script.js
+++ conversor-de-moedas-id3/dist/script.js
@@ -1,28 +1,60 @@
-var valorEmReal = document.getElementById('valorReal');
-var valorEmDolar = document.getElementById('valorDolar');
-var valorEmEuro = document.getElementById('valorEuro');
-var valorEmBitcoin = document.getElementById('valorBit');
+var moedaRecebida = document.getElementById("moedaRecebida");
+var valorMoedaRecebida = document.getElementById("valorMoedaRecebida");
+var moedaEntregue = document.getElementById("moedaEntregue");
+var resultado = document.getElementById("resultado");
 
-valorEmReal.addEventListener('input', function(event){
-  valorEmDolar.value = "US$ " + (valorEmReal.value / 5.24).toFixed(2) + "."; 
-  valorEmEuro.value =  "€ " + (valorEmReal.value / 6.19).toFixed(2) + "."; 
-  valorEmBitcoin.value = "₿ " + (valorEmReal.value / 252100.54).toFixed(2) + "."; 
-}, false)
+function converteReal(){
+  var quantidadeMoeda = valorMoedaRecebida.value;
+  if(moedaRecebida.value == "dolar") {
+    resultado.innerHTML = (quantidadeMoeda * 5.24).toFixed(2);
+  } else if (moedaRecebida.value == "euro"){
+    resultado.innerHTML = (quantidadeMoeda * 6.19).toFixed(2);
+  } else {
+    resultado.innerHTML = (quantidadeMoeda * 251568.23).toFixed(2);
+  }
+}
 
-valorEmDolar.addEventListener('input', function(event){
-  valorEmReal.value = "R$ " + (valorEmDolar.value * 5.24).toFixed(2) + ".";
-  valorEmEuro.value = "€ " + (valorEmDolar.value / 0.85).toFixed(2) + "."; 
-  valorEmBitcoin.value = "₿ " + (valorEmDolar.value / 0.000021).toFixed(2) + ".";
-}, false)
+function converteDolar(){
+  var quantidadeMoeda = valorMoedaRecebida.value;
+  if(moedaRecebida.value == "reais") {
+    resultado.innerHTML = (quantidadeMoeda / 5.24).toFixed(2);
+  } else if (moedaRecebida.value == "euro"){
+    resultado.innerHTML = (quantidadeMoeda * 1.18).toFixed(2);
+  } else {
+    resultado.innerHTML = (quantidadeMoeda * 48010.12).toFixed(2);
+  }
+}
 
-valorEmEuro.addEventListener('input', function(event){
-  valorEmDolar.value = "US$ " + (valorEmEuro.value * 1.18).toFixed(2) + ".";
-  valorEmReal.value =  "R$ " + (valorEmEuro.value * 6.19).toFixed(2) + ".";
-  valorEmBitcoin.value = "₿ " + (valorEmEuro.value / 0.000024).toFixed(2) + ".";
-}, false)
+function converteEuro(){
+   var quantidadeMoeda = valorMoedaRecebida.value;
+  if(moedaRecebida.value == "reais") {
+    resultado.innerHTML = (quantidadeMoeda / 6.19).toFixed(2);
+  } else if (moedaRecebida.value == "dolar"){
+    resultado.innerHTML = (quantidadeMoeda / 0.85).toFixed(2);
+  } else {
+    resultado.innerHTML = (quantidadeMoeda * 40685.23).toFixed(2);
+  }
+}
 
-valorEmBitcoin.addEventListener('input', function(event){
-  valorEmDolar.value = "US$ " + (valorEmBitcoin.value * 48010.12).toFixed(2) + ".";
-  valorEmEuro.value = "€ " + (valorEmBitcoin.value * 40685.23).toFixed(2) + ".";
-  valorEmReal.value = "R$ " + (valorEmBitcoin.value * 251568.23).toFixed(2) + ".";
-}, false)
+function converteBitcoin(){
+    var quantidadeMoeda = valorMoedaRecebida.value;
+  if(moedaRecebida.value == "reais") {
+    resultado.innerHTML = (quantidadeMoeda / 252100.54).toFixed(2);
+  } else if (moedaRecebida.value == "dolar"){
+    resultado.innerHTML = (quantidadeMoeda * 0.000021).toFixed(2);
+  } else {
+    resultado.innerHTML = (quantidadeMoeda * 0.000024).toFixed(2);
+  }
+}
+
+function converter(){
+  if(moedaEntregue.value == "reais") {
+    converteReal();
+  } else if (moedaEntregue.value == "dolar"){
+    converteDolar();    
+  } else if (moedaEntregue.value == "euro") {
+    converteEuro();
+  } else {
+    converteBitcoin();
+  }
+}
